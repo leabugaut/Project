778107f9f67d23495e9710b1146b39597874f94c
lea <lea.bugaut@edu.devinci.fr>
2023-03-25 17:16:41 +0000

    Ajout de texte et dernière population enregistrée

---
+++ population.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
+import pandas as pd
+from pathlib import Path
+import subprocess
+import datetime as dt
+import plotly.express as px
+
+# Création de l'interface dashboard
+app = dash.Dash(__name__)
+app.title = "Population française"
+
+# Layout du dashboard
+app.layout = html.Div(children=[
+    html.H1("Population française", style={"text-align": "center", "color":"blue"}),
+    html.P("Pendant l'année 2023, la population française devrait augmenter de 297 388 personnes pour atteindre 66 383 596 au début de l'année 2024. L'augmentation naturelle devrait être positive, car le nombre de naissances dépassera le nombre de décès de 228 658. Si la migration externe reste au même niveau que l'année précédente, la population augmentera de 68 730 en raison de raisons liées à la migration. Cela signifie que le nombre de personnes qui s'installent en France (et qui ne sont pas originaires du pays) en tant que résidents permanents (immigrants) l'emportera sur le nombre de personnes qui quittent le pays pour s'installer définitivement dans un autre pays (émigrants)"), 
+    html.Div(id="data-update", style = {"color":"blue", 'font-weight': 'bold'}),
+    dcc.Interval(id="interval-component", interval=60*1000, n_intervals=0),
+], style={'backgroundColor': '#B0E0E6'})
+
+
+# Fonction pour mettre à jour les données du dashboard 
+@app.callback(
+    dash.dependencies.Output("data-update", "children"),
+    dash.dependencies.Input("interval-component", "n_intervals")
+)
+
+
+# Lecture du fichier csv pour récupérer les dernières données 
+def data_retrieval(n):
+    df = pd.read_csv('data.csv')
+    df = df.loc[:, ['timestamp', 'pop']]
+    last_pop = df['pop'].iloc[-1]
+    return f'Dernière population enregistrée : {last_pop}'
+
+
+
+
+# Exécution du dashboard  
+if __name__ == '__main__':
+   app.run_server(host='0.0.0.0', port=8050, debug=True)
+
+
